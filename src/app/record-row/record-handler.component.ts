0f93ec2fedbf6466e71a57298080f5103e041f0d
Ricardo Zavala <rzavala@tibco-support.com>
2019-05-29 22:15:39 -0500

    Record row with checkbox

--- src/app/record-row/record-handler.component.ts
+++ src/app/record-row/record-handler.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, ElementRef, ViewChild } from "@angular/core";
 import { Switch } from "tns-core-modules/ui/switch";
 import { EventData } from "tns-core-modules/data/observable";
 import { CCRecord } from "../../models";
@@ -10,10 +10,12 @@ import { CollectionService } from "../services/collection.service";
 })
 export class RecordHandlerComponent implements OnInit {
     @Input() cc: CCRecord;
+    @ViewChild("cb") checkbox: ElementRef;
+
     public checkState = { checked: false };
     public emmitUpdates = false;
 
-    private initialized = false;
+    initialized = false;
 
     constructor(public db: CollectionService) { }
 
@@ -21,39 +23,25 @@ export class RecordHandlerComponent implements OnInit {
         this.checkState.checked = this.cc.checked;
     }
 
-    // checkUpdate($event) {
-    //     console.log($event);
-
-    //     if (($event.target && $event.target.checked) || ($event.detail && $event.detail.checked)) {
-    //         this.cc.check();
-    //         this.db.updateRecord(this.cc, this.emmitUpdates);
-    //     } else {
-    //         this.db.uncheck(this.cc, this.emmitUpdates)
-    //             .subscribe(
-    //                 unchecked => {
-    //                     if (!unchecked) {
-    //                         this.checkState.checked = true;
-    //                     }
-    //                 }
-    //             );
-    //     }
-    // }
-
-    checkUpdate(args: EventData) {
+    checkUpdate() {
         if (this.checkState.checked && !this.initialized) {
             this.initialized = true;
             return;
         }
 
-        const switchEle = args.object as Switch;
-        this.checkState.checked = switchEle.checked;
-
-        if (switchEle.checked) {
+        if (!this.checkState.checked) {
+            this.checkState.checked = true;
             this.cc.check();
             this.db.updateRecord(this.cc, this.emmitUpdates);
         } else {
             this.db.uncheck(this.cc, this.emmitUpdates)
-                .subscribe(unchecked => this.checkState.checked = !unchecked);
+                .subscribe(unchecked => {
+                    this.checkState.checked = !unchecked;
+                    if (!unchecked) {
+                        this.initialized = false; // To avoid double call
+                        this.checkbox.nativeElement.toggle();
+                    }
+                });
         }
     }
 }
